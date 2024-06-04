89baeeefeb0493694198268bd876a39d2515dbf7
mihirb1234 <67053200+mihirb1234@users.noreply.github.com>
2024-06-04 20:09:48 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const app = express();
+dotenv.config();
+app.use(cors());
+
+const auth=require("./routes/auth")
+const quoteRoutes=require('./routes/quoteRoutes')
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("mongodb connected"))
+  .catch((err) => console.log(err));
+
+app.use(express.json({ extended: false }));
+app.use('/api/auth',auth);
+app.use('/api/quotes',quoteRoutes);
+
+const PORT=process.env.PORT||5000;
+app.listen(PORT,()=>console.log(`server running on port ${PORT}`));
